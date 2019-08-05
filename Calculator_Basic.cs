b348c12dc14aaacbc22335dc2e34af034f5e37dd
brandonhost <53795103+brandonhost@users.noreply.github.com>
2019-08-06 00:18:29 +0300

    Update Calculator_Basic.cs

--- Calculator_Basic.cs
+++ Calculator_Basic.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,44 +8,58 @@ namespace Calculator_Basic
 {
     class Program
     {
+        private static readonly string[] operators = { "+", "-", "*", "/", "%" };
+
         static void Main(string[] args)
         {
             while (true)
             {
                 Console.WriteLine("Enter first number");
                 double num1;
-                while (!double.TryParse(Console.ReadLine(), out num1)) {
+                while (!double.TryParse(Console.ReadLine(), out num1))
+                {
                     Console.WriteLine("Invalid number");
                 }
 
                 Console.WriteLine("Enter second number");
                 double num2;
-                while (!double.TryParse(Console.ReadLine(), out num2)) {
+                while (!double.TryParse(Console.ReadLine(), out num2))
+                {
                     Console.WriteLine("Invalid number");
                 }
 
-                Console.WriteLine("Enter operator (+, -, *, /)");
+                Console.WriteLine("Enter operator (" + string.Join(", ", operators) + ")");
+
                 string op = Console.ReadLine();
+                while (Array.IndexOf(operators, op) == -1)
+                {
+                    Console.WriteLine("Invalid operator, try again: ");
+                    op = Console.ReadLine();
+                }
 
                 Console.Write("Answer: ");
                 switch (op)
                 {
                     case "+":
-                        Console.WriteLine(num1 + num2);
+                        Console.Write(num1 + num2);
                         break;
                     case "-":
-                        Console.WriteLine(num1 - num2);
+                        Console.Write(num1 - num2);
                         break;
                     case "*":
-                        Console.WriteLine(num1 * num2);
+                        Console.Write(num1 * num2);
                         break;
                     case "/":
-                        Console.WriteLine(num1 / num2);
+                        Console.Write(num1 / num2);
                         break;
+                    //case "%":
+                    //    Console.Write(num1 % num2);
+                    //    break;
                     default:
-                        Console.WriteLine("Incorrect operator");
+                        Console.Write("Operator not implemented yet.");
                         break;
                 }
+                Console.WriteLine("\n------------------------");
             }
         }
     }
